104babe39ffaadd0e0a68c7906cb23ab9911c8f6
Mike Vengerovich <mikeveng73@gmail.com>
2024-06-22 03:57:23 +0300

    - in progress

--- apps/rmdir/rmdir.c
+++ apps/rmdir/rmdir.c
@@ -1,36 +1,32 @@
 #include "m-os-api.h"
 
 static int rmdir(const char* d) {
-    DIR dir;
-    FILINFO fileInfo;
+    DIR* dir = (DIR*)pvPortMalloc(sizeof(DIR));
     if (FR_OK != f_opendir(&dir, d)) {
-        cmd_startup_ctx_t* ctx = get_cmd_startup_ctx();
-        fgoutf(ctx->pstderr, "Failed to open directory: '%s'\n", d);
+        vPortFree(dir);
+        fgoutf(get_cmd_startup_ctx()->pstderr, "Failed to open directory: '%s'\n", d);
         return 0;
     }
     size_t total_files = 0;
-    char* t = (char*)pvPortMalloc(512);
-    while (f_readdir(&dir, &fileInfo) == FR_OK && fileInfo.fname[0] != '\0') {
-        size_t s = strlen(d);
-        strncpy(t, d, 512);
-        t[s] = '/';
-        strncpy(t + s + 1, fileInfo.fname, 511 - s);
-        if(fileInfo.fattrib & AM_DIR) {
+    FILINFO* fileInfo = (FILINFO*)pvPortMalloc(sizeof(FILINFO));
+    while (f_readdir(dir, &fileInfo) == FR_OK && fileInfo->fname[0] != '\0') {
+        char* t = concat(d, fileInfo->fname);
+        if(fileInfo->fattrib & AM_DIR) {
             total_files += rmdir(t);
         }
         if (f_unlink(t) == FR_OK)
             total_files++;
         else {
-            cmd_startup_ctx_t* ctx = get_cmd_startup_ctx();
-            fgoutf(ctx->pstderr, "Failed to remove file: '%s'\n", t);
+            fgoutf(get_cmd_startup_ctx()->pstderr, "Failed to remove file: '%s'\n", t);
         }
+        vPortFree(t);
     }
-    vPortFree(t);
-    f_closedir(&dir);
+    vPortFree(fileInfo);
+    f_closedir(dir);
+    vPortFree(dir);
     if (f_unlink(d) == FR_OK) {
         total_files++;
     }
-    goutf("    Total: %d files removed\n", total_files);
     return total_files;
 }
 
@@ -41,6 +37,11 @@ int main() {
         return 1;
     }
     char * d = (char*)ctx->cmd + (next_token(ctx->cmd_t) - ctx->cmd_t);
-    rmdir(d);
+    int files = rmdir(d);
+    goutf("    Total: %d files removed\n", files);
     return 0;
+}
+
+int __required_m_api_verion(void) {
+    return 2;
 }
