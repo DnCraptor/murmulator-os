e31d60633cb14d394ff8d1450614905b3b9f2455
Mike Vengerovich <mikeveng73@gmail.com>
2024-06-24 21:55:42 +0300

    + cls

--- apps/cls/cls.c
+++ apps/cls/cls.c
@@ -1,11 +1,12 @@
 #include "m-os-api.h"
 
-int main() {
+int main(void) {
     clrScr(1);
     graphics_set_con_pos(0, 1);
     graphics_set_con_color(7, 0);
+    return 0;
 }
 
 int __required_m_api_verion(void) {
-    return 2;
+    return M_API_VERSION;
 }
