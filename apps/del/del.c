8e97a6a200994caa4555cba5d696488302682e5f
Mike Vengerovich <mikeveng73@gmail.com>
2024-06-25 18:16:38 +0300

    + rm + ls

--- apps/del/del.c
+++ apps/del/del.c
@@ -1,32 +1,34 @@
 #include "m-os-api.h"
 
-FILINFO fi;
-int main() {
-    cmd_startup_ctx_t* ctx = get_cmd_startup_ctx();
-    if (ctx->tokens == 1) {
-        fgoutf(ctx->pstderr, "Unable to remove nothing\n");
+int main(void) {
+    cmd_ctx_t* ctx = get_cmd_ctx();
+    if (ctx->argc == 1) {
+        fgoutf(ctx->std_err, "Unable to remove nothing\n");
         return 1;
-    } else if (ctx->tokens == 2) {
-        char *f = (char*)ctx->cmd + (next_token(ctx->cmd_t) - ctx->cmd_t);
-        if (f_stat(f, &fi) != FR_OK) {
-            fgoutf(ctx->pstderr, "File '%s' not found\n", f);
+    } else if (ctx->argc == 2) {
+        FILINFO* pfi = malloc(sizeof(FILINFO));
+        if (f_stat(ctx->argv[1], pfi) != FR_OK) {
+            fgoutf(ctx->std_err, "File '%s' not found\n", ctx->argv[1]);
+            free(pfi);
             return 2;
         }
-        if (fi.fattrib & AM_DIR) {
-            fgoutf(ctx->pstderr, "WARN: '%s' is a folder\n", f);
+        if (pfi->fattrib & AM_DIR) {
+            fgoutf(ctx->std_err, "WARN: '%s' is a folder\n", ctx->argv[1]);
         }
-        if (f_unlink(f) != FR_OK) {
-            fgoutf(ctx->pstderr, "Unable to unlink: '%s'\n", f);
+        if (f_unlink(ctx->argv[1]) != FR_OK) {
+            fgoutf(ctx->std_err, "Unable to unlink: '%s'\n", ctx->argv[1]);
+            free(pfi);
             return 4;
         }
-        fgoutf(ctx->pstdout, "Unlink '%s' passed\n", f);
+        free(pfi);
+        fgoutf(ctx->std_out, "Unlink '%s' passed\n", ctx->argv[1]);
     } else {
-        fgoutf(ctx->pstderr, "Unable to remove more than one file\n");
+        fgoutf(ctx->std_err, "Unable to remove more than one file\n");
         return 5;
     }
     return 0;
 }
 
-int __required_m_api_verion() {
-    return 2;
+int __required_m_api_verion(void) {
+    return M_API_VERSION;
 }
--- apps/dir/dir.c
+++ apps/dir/dir.c
@@ -1,38 +1,38 @@
 #include "m-os-api.h"
 
 int main(void) {
-    cmd_startup_ctx_t* ctx = get_cmd_startup_ctx();
-    char* d = ctx->tokens == 1 ? ctx->curr_dir : (char*)ctx->cmd + (next_token(ctx->cmd_t) - ctx->cmd_t);
+    cmd_ctx_t* ctx = get_cmd_ctx();
+    char* d = ctx->argc == 1 ? ctx->curr_dir : ctx->argv[1];
     DIR* pdir = (DIR*)pvPortMalloc(sizeof(DIR));
     if (FR_OK != f_opendir(pdir, d)) {
         vPortFree(pdir);
-        fgoutf(ctx->pstderr, "Failed to open directory: '%s'\n", d);
+        fgoutf(ctx->std_err, "Failed to open directory: '%s'\n", d);
         return;
     }
     if (strlen(d) > 1) {
-        fgoutf(ctx->pstdout, "D ..\n");
+        fgoutf(ctx->std_out, "D ..\n");
     }
     int total_files = 0;
     FILINFO* pfileInfo = (FILINFO*)pvPortMalloc(sizeof(FILINFO));
     while (f_readdir(pdir, pfileInfo) == FR_OK && pfileInfo->fname[0] != '\0') {
-        fgoutf( ctx->pstdout, (pfileInfo->fattrib & AM_DIR) ? "D " : "  ");
+        fgoutf( ctx->std_out, (pfileInfo->fattrib & AM_DIR) ? "D " : "  ");
         if ((pfileInfo->fsize >> 20) > 9) {
-            fgoutf( ctx->pstdout, "%04dM",  (uint32_t)(pfileInfo->fsize >> 20));
+            fgoutf( ctx->std_out, "%04dM",  (uint32_t)(pfileInfo->fsize >> 20));
         } else if ((pfileInfo->fsize >> 10) > 9) {
-            fgoutf( ctx->pstdout, "%04dK",  (uint32_t)(pfileInfo->fsize >> 10));
+            fgoutf( ctx->std_out, "%04dK",  (uint32_t)(pfileInfo->fsize >> 10));
         } else {
-            fgoutf( ctx->pstdout, "%04dB",  (uint32_t)pfileInfo->fsize);
+            fgoutf( ctx->std_out, "%04dB",  (uint32_t)pfileInfo->fsize);
         }
-        fgoutf( ctx->pstdout, " %s\n", pfileInfo->fname);
+        fgoutf( ctx->std_out, " %s\n", pfileInfo->fname);
         total_files++;
     }
     vPortFree(pfileInfo);
     f_closedir(pdir);
     vPortFree(pdir);
-    fgoutf(ctx->pstdout, "    Total: %d files.\n", total_files);
+    fgoutf(ctx->std_out, "    Total: %d files.\n", total_files);
     return 0;
 }
 
 int __required_m_api_verion(void) {
-    return 2;
+    return M_API_VERSION;
 }
