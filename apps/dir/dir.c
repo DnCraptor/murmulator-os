12af795134cd7ad4d0e0732e764815579e41bd8b
Mike Vengerovich <mikeveng73@gmail.com>
2024-08-15 22:59:40 +0300

    + ls/dir improvements

--- apps/dir/dir.c
+++ apps/dir/dir.c
@@ -3,33 +3,37 @@
 int main(void) {
     cmd_ctx_t* ctx = get_cmd_ctx();
     char* d = ctx->argc == 1 ? get_ctx_var(ctx, "CD") : ctx->argv[1];
-    DIR* pdir = (DIR*)pvPortMalloc(sizeof(DIR));
+    DIR* pdir = (DIR*)malloc(sizeof(DIR));
     if (FR_OK != f_opendir(pdir, d)) {
-        vPortFree(pdir);
-        fgoutf(ctx->std_err, "Failed to open directory: '%s'\n", d);
+        free(pdir);
+        fprintf(ctx->std_err, "Failed to open directory: '%s'\n", d);
         return;
     }
     if (strlen(d) > 1) {
-        fgoutf(ctx->std_out, "D ..\n");
+        printf("D ..\n");
     }
     int total_files = 0;
-    FILINFO* pfileInfo = (FILINFO*)pvPortMalloc(sizeof(FILINFO));
+    FILINFO* pfileInfo = (FILINFO*)malloc(sizeof(FILINFO));
     while (f_readdir(pdir, pfileInfo) == FR_OK && pfileInfo->fname[0] != '\0') {
-        fgoutf( ctx->std_out, (pfileInfo->fattrib & AM_DIR) ? "D " : "  ");
-        if ((pfileInfo->fsize >> 20) > 9) {
-            fgoutf( ctx->std_out, "%04dM",  (uint32_t)(pfileInfo->fsize >> 20));
+        char tmp[] = ".....";
+        if (pfileInfo->fattrib & AM_DIR) {
+            // do nothing
+        } else if ((pfileInfo->fsize >> 20) > 9) {
+            snprintf(tmp, 6, "%04dM", (uint32_t)(pfileInfo->fsize >> 20));
         } else if ((pfileInfo->fsize >> 10) > 9) {
-            fgoutf( ctx->std_out, "%04dK",  (uint32_t)(pfileInfo->fsize >> 10));
+            snprintf(tmp, 6, "%04dK", (uint32_t)(pfileInfo->fsize >> 10));
         } else {
-            fgoutf( ctx->std_out, "%04dB",  (uint32_t)pfileInfo->fsize);
+            snprintf(tmp, 6, "%04dB", (uint32_t)pfileInfo->fsize);
         }
-        fgoutf( ctx->std_out, " %s\n", pfileInfo->fname);
+        char* t = tmp;
+        while(*t == '0') { *t++ = ' '; }
+        printf("%s %s %s\n", (pfileInfo->fattrib & AM_DIR) ? "D" : " ", tmp, pfileInfo->fname);
         total_files++;
     }
-    vPortFree(pfileInfo);
+    free(pfileInfo);
     f_closedir(pdir);
-    vPortFree(pdir);
-    fgoutf(ctx->std_out, "    Total: %d files.\n", total_files);
+    free(pdir);
+    printf("    Total: %d files.\n", total_files);
     return 0;
 }
 
