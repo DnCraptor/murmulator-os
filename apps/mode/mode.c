9977533eb2bf483643127cdd41325a6f51c03678
Mike Vengerovich <mikeveng73@gmail.com>
2024-08-26 23:47:54 +0300

    + mode - show current mode info

--- apps/mode/mode.c
+++ apps/mode/mode.c
@@ -2,15 +2,22 @@
 
 int main(void) {
     cmd_ctx_t* ctx = get_cmd_ctx();
-    if (ctx->argc != 2) {
+    if (ctx->argc == 1) {
+        int mode = graphics_get_mode();
+        printf("  Current mode: %d\n", mode);
+        bool b = graphics_is_mode_text(mode);
+        printf("     text only: %s\n", b ? "true" : "false");
+        printf("    resolution: %dx%dx%d-bit\n", get_screen_width(), get_screen_height(), get_screen_bitness());
+        if (!b) {
+            printf("txt resolution: %dx%d\n", get_console_width(), get_console_height());
+        }
+        return 0;
+    }
+    if (ctx->argc > 2) {
         fprintf(ctx->std_err, "Usage: mode #\n where # - integer number\n");
         return 1;
     }
     int mode = atoi(ctx->argv[1]);
-//    if (!graphics_is_mode_text(mode)) {
-//        fprintf(ctx->std_err, "mode #%d is not ready for text output\n", mode);
-//        return -2;
-//    }
     if (!graphics_set_mode(mode)) {
         fprintf(ctx->std_err, "Unsupported mode #%d\n", mode);
         return -1;
