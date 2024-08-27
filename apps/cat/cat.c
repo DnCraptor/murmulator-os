6283b3b432c128b90a2484e2681e8597e42269cf
Mike Vengerovich <mikeveng73@gmail.com>
2024-08-27 09:10:05 +0300

    + cat - sigkill support

--- apps/cat/cat.c
+++ apps/cat/cat.c
@@ -1,5 +1,7 @@
 #include "m-os-api.h"
 
+volatile bool marked_to_exit;
+
 int main() {
     cmd_ctx_t* ctx = get_cmd_ctx();
     if (ctx->argc == 0) {
@@ -23,7 +25,8 @@ int main() {
     }
     UINT rb;
     char* buf = (char*)pvPortMalloc(300);
-    while(f_read(f, buf, 299, &rb) == FR_OK && rb > 0) {
+    marked_to_exit = false;
+    while(f_read(f, buf, 299, &rb) == FR_OK && rb > 0 && !marked_to_exit) {
         buf[rb] = 0;
         fgoutf(ctx->std_out, buf);
     }
@@ -35,4 +38,9 @@ int main() {
 
 int __required_m_api_verion(void) {
     return M_API_VERSION;
+}
+
+// only SIGKILL is supported for now
+int signal(void) {
+	marked_to_exit = true;
 }
